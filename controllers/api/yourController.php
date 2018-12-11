0e5b9db619efb755c8a595a32d75700017abffaf
Dr Andrew Bingham <a.p.bingham@tees.ac.uk>
2018-12-11 12:51:40 +0000

    Create yourController.php

---
+++ controllers/api/yourController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controllers\api;
+
+use yii\rest\ActiveController;
+
+class PropertiesController extends ActiveController{
+    public $modelClass = 'app\models\Properties';
+}
